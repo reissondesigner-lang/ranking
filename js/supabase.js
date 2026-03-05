a87061b5c22f4f76c0ebc86fd29f1218d74c8b11
reissondesigner-lang <reissondesigner@gmail.com>
2026-03-05 10:25:26 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dhcmkldaqkbbbmqtsvpz.supabase.co"
+const SUPABASE_KEY = "sb_publishable_MmYYuWp95eLSIdGI4Kz-Mg_a7SLa2-G"
+
+const db = window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+)
